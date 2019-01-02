ec79df6f44e2b16b653abc961034eba193d5f129
Enrique Ramírez <me@enrique-ramirez.com>
2019-01-02 12:54:28 +0100

    Migrating from postcss-cssnext to postcss-preset-env

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,12 @@
 module.exports = {
   plugins: {
     'postcss-import': {},
-    'postcss-cssnext': {
+    'postcss-preset-env': {
+      stage: 3,
       browsers: ['last 2 major versions', '> 5%', 'ie 11'],
+      features: {
+        'color-mod-function': { unresolved: 'warn' },
+      },
     },
   },
 }
